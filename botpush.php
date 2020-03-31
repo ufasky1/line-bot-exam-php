be51f38d14a78b2266ece96fe36fcc94b63798fe
ufasky1 <62899545+ufasky1@users.noreply.github.com>
2020-03-31 13:37:52 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'ofsgNCuuR6OONh/o59md7/a7q+8vsFTIU+nr01ecw1UWvjohJGfaaXStct9+hM6dcieapf9kqLwFjl7Usodf0iAS7v1cAb6IYBxOgxGVYPrwtcICEErPRDUxypQdrQ+UORGcBkCPt4pCcYxNWUO4+gdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ofsgNCuuR6OONh/o59md7/afthfghfhfjhk7tsfdsytrsghsfsodf0iAS7v1cAb6IYBxOgxGVYPrwtcICEErPRDUxypQdrQ+UORGcBkCPt4pCcYxNWUO4+gdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '932fd7fa9d4d7b5dc9bff68176ea3803';
+$channelSecret = '932fd7fa9d4d7b5dc9bh;h;fgyt176ea3803';
 
-$pushID = 'Ube12b6d42e053b792179d99232e63439';
+$pushID = 'Ube12b6d42e053b792179dlhhk2e63439';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
