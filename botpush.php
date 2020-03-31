e34d31101f2919f9d9cd3a6840ea25fb7774f61a
ufasky1 <62899545+ufasky1@users.noreply.github.com>
2020-03-31 12:00:32 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -15,7 +15,9 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
 
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
 
