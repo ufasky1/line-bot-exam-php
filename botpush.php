6c660fcc473c25e27b78170f2fd34f6c14d88172
ufasky1 <62899545+ufasky1@users.noreply.github.com>
2020-03-31 13:18:30 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -15,7 +15,7 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 			$columns = array();
 			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-			for($i=0;$i<5;$i++) {
+			for($i=0;$i<1;$i++) {
 				$actions = array(
 					new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Add to Card","action=carousel&button=".$i),
 					new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("View","http://www.google.com")
